33cd0eaf0ceadd12cc401fc2688a5b44747b25ed
caszofficial <santiagozapata07@gmail.com>
2021-07-06 17:03:46 -0500

    WIP CreateUSerForm.jsx  finished error validations

--- client/src/components/Users/UserAdd/CreateUserForm.jsx
+++ client/src/components/Users/UserAdd/CreateUserForm.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import TextField from "@material-ui/core/TextField";
 import { makeStyles } from "@material-ui/core";
 import { Hidden, Button } from "@material-ui/core";
@@ -19,11 +19,12 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: "column",
     margin: "20px",
     boxShadow: "0 4px 5px black",
-    padding: "50px",
+    padding: "20px",
     borderRadius: "20px",
   },
   input1: {
     display: "flex",
+    justifyContent: "space-between",
   },
   inputs: {
     margin: "20px",
@@ -35,8 +36,133 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export const CreateUserForm = () => {
+const CreateUserForm = () => {
+  const [input, setInput] = useState({});
+  const [error, setError] = useState({
+    //Control the error red border of the inputs
+    name: false,
+    username: false,
+    email: false,
+    password: false,
+    contact: false,
+    isDeleted: false,
+  });
+
+  const [helperText, setHelperText] = useState({
+    //Control the warning message
+    name: "Ingrese un Nombre",
+    username: "Ingrese un nombre de usuario válido",
+    email: "Ingrese un email válido",
+    password: "Debe contener al menos un numero, una mayuscula y 8 caracteres",
+    contact: "Numero de Telefono",
+    isDeleted: "Ingrese un is deleted",
+  });
+
+  const validate = (field) => {
+    switch (field.name) {
+      case "username":
+        if (!/^([A-Za-z0-9]){4,20}$/.test(field.value)) {
+          setError({ ...error, username: true });
+          if (field.value.length < 4) {
+            setHelperText({
+              ...helperText,
+              username: "Nombre de usuario muy corto",
+            });
+          } else if (field.value.length > 20) {
+            setHelperText({
+              ...helperText,
+              username: "Nombre de usuario demasiado largo",
+            });
+          } else {
+            setHelperText({
+              ...helperText,
+              username: "Contiene caracteres no aceptados",
+            });
+          }
+        } else {
+          setError({ ...error, username: false });
+          setHelperText({ ...helperText, username: "" });
+        }
+        break;
+
+      case "name":
+        if (!/^[A-Za-z .'-]{3,20}$/.test(field.value)) {
+          setError({ ...error, name: true });
+          if (field.value.length < 3) {
+            setHelperText({ ...helperText, name: "Es muy corto" });
+          } else if (field.value.length > 20) {
+            setHelperText({ ...helperText, name: "Es muy largo" });
+          } else {
+            setHelperText({
+              ...helperText,
+              name: "No se permiten caracteres especiales",
+            });
+          }
+        } else {
+          setError({ ...error, name: false });
+          setHelperText({ ...helperText, name: "" });
+        }
+        break;
+      case "email":
+        if (
+          !/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/.test(
+            field.value
+          )
+        ) {
+          setError({ ...error, name: true });
+          if (field.value.length < 3) {
+            setHelperText({ ...helperText, email: "Es muy corto" });
+          } else if (field.value.length > 40) {
+            setHelperText({ ...helperText, email: "Es muy largo" });
+          } else {
+            setHelperText({
+              ...helperText,
+              email: "Ingrese un email válido",
+            });
+          }
+        } else {
+          setError({ ...error, email: false });
+          setHelperText({ ...helperText, email: "" });
+        }
+        break;
+      case "password":
+        if (
+          !/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,60}$/.test(
+            field.value
+          )
+        ) {
+          setError({ ...error, password: true });
+          if (field.value.length < 8) {
+            setHelperText({ ...helperText, password: "Es muy corto" });
+          } else if (field.value.length > 60) {
+            setHelperText({ ...helperText, password: "Es muy largo" });
+          } else {
+            setHelperText({
+              ...helperText,
+              password: "Password",
+            });
+          }
+        } else {
+          setError({ ...error, password: false });
+          setHelperText({ ...helperText, password: "" });
+        }
+        break;
+    }
+  };
+
+  useEffect(() => {
+    validate("email");
+  }, [error]);
+
+  const handleInputChange = function (e) {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
+    validate(e.target);
+  };
   const classes = useStyles();
+
   return (
     <div className={classes.main}>
       <Hidden only={["sm", "xs"]}>
@@ -45,21 +171,38 @@ export const CreateUserForm = () => {
             <TextField
               id="standard-basic"
               label="Nombre"
+              name="name"
               className={classes.inputs}
-              color="secondary"
+              autoComplete="new"
+              value={input.name}
+              onChange={handleInputChange}
+              error={error["name"]}
+              helperText={[helperText["name"]]}
             />
 
             <TextField
               id="standard-basic"
               label="Nombre de usuario"
+              name="username"
               className={classes.inputs}
+              value={input.username}
+              autoComplete="new"
+              onChange={handleInputChange}
+              error={error["username"]}
+              helperText={[helperText["username"]]}
             />
           </div>
 
           <TextField
             id="standard-basic"
+            name="email"
             label="Email"
             className={classes.inputs}
+            error={error["email"]}
+            value={input.email}
+            autoComplete="off"
+            onChange={handleInputChange}
+            helperText={[helperText["email"]]}
           />
 
           <div className={classes.input1}>
@@ -67,7 +210,12 @@ export const CreateUserForm = () => {
               id="standard-basic"
               label="Contraseña"
               type="password"
+              name="password"
               className={classes.inputs}
+              value={input.password}
+              onChange={handleInputChange}
+              error={error["password"]}
+              helperText={[helperText["password"]]}
             />
             <TextField
               id="standard-multiline-flexible"
@@ -91,27 +239,50 @@ export const CreateUserForm = () => {
           <TextField
             id="standard-basic"
             label="Nombre"
+            name="name"
             className={classes.inputs}
-            color="secondary"
+            autoComplete="new"
+            value={input.name}
+            onChange={handleInputChange}
+            error={error["name"]}
+            helperText={[helperText["name"]]}
           />
 
           <TextField
             id="standard-basic"
+            name="username"
             label="Nombre de usuario"
             className={classes.inputs}
+            value={input.username}
+            autoComplete="new"
+            onChange={handleInputChange}
+            error={error["username"]}
+            helperText={[helperText["username"]]}
           />
 
           <TextField
             id="standard-basic"
             label="Email"
+            name="email"
+            autoComplete="new"
             className={classes.inputs}
+            error={error["email"]}
+            value={input.email}
+            autoComplete="off"
+            onChange={handleInputChange}
+            helperText={[helperText["email"]]}
           />
 
           <TextField
             id="standard-basic"
             label="Contraseña"
+            name="password"
             type="password"
             className={classes.inputs}
+            value={input.password}
+            onChange={handleInputChange}
+            error={error["password"]}
+            helperText={[helperText["password"]]}
           />
           <TextField
             id="standard-multiline-flexible"
