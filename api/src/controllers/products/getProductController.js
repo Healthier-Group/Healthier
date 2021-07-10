1d75bbdb6b6e5f6332429eb36b48c0281daef5ae
caszofficial <santiagozapata07@gmail.com>
2021-07-10 18:07:19 -0500

    search implemented on getProduct controller

--- api/src/controllers/products/getProductController.js
+++ api/src/controllers/products/getProductController.js
@@ -1,11 +1,22 @@
 const { Product } = require("../../db");
+const {Op} = require('sequelize')
 
 module.exports = async (req, res, next) => {
   try {
-    const products = await Product.findAll();
-    return res.json(products);
+    const { q } = req.query;
+    if (!q) {
+      await Product.findAll()
+      .then(product => res.send(product))
+    } else {
+      await Product.findAll({
+        where:{
+          name:{[Op.iLike]: `%${q}%`}
+        }
+      })
+      .then(product => res.send(product))
+    }
+    
   } catch (error) {
-    next(error);
-    return res.json(error);
+    next(error)
   }
 };
