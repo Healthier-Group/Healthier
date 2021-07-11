ec0e5e219d0ceb898f8e58362296bb570e0116d2
caszofficial <santiagozapata07@gmail.com>
2021-07-11 03:01:34 -0500

    Fixed Drawer on NavBar

--- client/src/AppGlobal.jsx
+++ client/src/AppGlobal.jsx
@@ -1,27 +1,28 @@
-import './App.css'
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
-import {ThemeProvider} from '@material-ui/core/styles'
-import theme from './utils/Theme.js'
-import Container from './Components/Container/Container'
-import ProductCard from './Components/Products/ProductCard'
-import ProductDetail from './Components/Products/ProductDetail'
-import CreateProductForm from './Components/Products/CreateProductForm'
-import Recipes from './Components/Recipes/Recipes'
+import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { ThemeProvider } from "@material-ui/core/styles";
+import theme from "./utils/Theme.js";
+import Container from "./Components/Container/Container";
+import ProductCard from "./Components/Products/ProductCard";
+import ProductDetail from "./Components/Products/ProductDetail";
+import CreateProductForm from "./Components/Products/CreateProductForm";
+import Recipes from "./Components/Recipes/Recipes";
 
-function AppGlobal(){
-	return(
-		<ThemeProvider theme={theme}>
-			<BrowserRouter>
-				<Switch> 
-					<Route path='/' exact component={Container}/>
-					<Route path='/recipes' exact component={Recipes}/>
-					<Route path ='/products/:id' component={ProductDetail} />
-					<Route path ='/products' component={ProductCard}/>
-					<Route path='/form' component={CreateProductForm}/>
-				</Switch>
-			</BrowserRouter>
-		</ThemeProvider>
-	)
+
+function AppGlobal() {
+  return (
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={Container} />
+          <Route path="/recipes" exact component={Recipes} />
+          <Route path="/products/:id" component={ProductDetail} />
+          <Route path="/products" component={ProductCard} />
+          <Route path="/form" component={CreateProductForm} />
+        </Switch>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 }
 
 export default AppGlobal;
--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,77 +1,100 @@
-import React, {useState} from 'react'
-import {AppBar, Toolbar, Typography, makeStyles, Button, IconButton, Hidden} from '@material-ui/core'
-import {Link} from 'react-router-dom'
-import MenuIcon from '@material-ui/icons/Menu'
-import SearchBar from '../SearchBar/SearchBar'
-import Logo from '../../Images/h.png'
-import DrawerApp from '../DrawerApp/DrawerApp'
+import React, { useState } from "react";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  makeStyles,
+  Button,
+  IconButton,
+  Hidden,
+  Drawer,
+  Divider,
+} from "@material-ui/core";
+import { Link } from "react-router-dom";
+import MenuIcon from "@material-ui/icons/Menu";
+import SearchBar from "../SearchBar/SearchBar";
+import Logo from "../../Images/h.png";
 
-const useStyles = makeStyles(theme => ({
-    menuButton:{
-        marginRight: theme.spacing(2),
-        [theme.breakpoints.up('sm')]: {display: 'none'}
-    },
-    title:{
-        flexGrow: 1,
-        fontFamily: 'Merienda One'
-    },
-    image:{
-        borderRadius: '50%',
-        width: '5%',
-        marginBottom: '-1.5%',
-        marginRight: '2%'
-    },
-    buttons:{
-        textDecoration: 'none',
-        marginRight: '2%'
-    }
-}))
+import List from "../List/List";
 
-export default function NavBar(){
-    const classes = useStyles()
-    const[openApp, setOpenApp] = useState(true)
-    function openAction(){
-        setOpenApp(!openApp)
-    }
-    return(
-        <AppBar className = {classes.appBar}>
-            <Toolbar>
-                <Typography variant = 'h6'  className = {classes.title}>
-                    <Link to = '/'>
-                        <img src = {Logo} className = {classes.image}/>
-                    </Link>
-                        Healthier Club
-                </Typography>
-                <Link to = '/products' className = {classes.buttons}>
-                    <Button color = 'secondary'>
-                        Productos
-                    </Button>
-                </Link>
-                <Link to = '/recipes' className = {classes.buttons}>
-                    <Button color = 'secondary'>
-                        Recetas
-                    </Button>
-                </Link>
-                <Hidden only = {['xs', 'sm']}>
-                    <SearchBar/>
-                </Hidden>
-                <Hidden only = {['md', 'lg', 'xl']}>
-                    <IconButton color = 'secondary'
-                                onClick = {(e) => openAction(e)}>
-                        <MenuIcon />
-                    </IconButton>
-                </Hidden>
-                <Hidden>
-                <DrawerApp variant = 'permanent'
-                            open = {true} />
-           </Hidden>
-           <Hidden>
-                <DrawerApp variant = 'permanent'
-                            open = {openApp}
-                            onClose = {(e) => openAction(e)} />
-           </Hidden>
-            </Toolbar>
-        </AppBar>
-    )
+const useStyles = makeStyles((theme) => ({
+  menuButton: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up("sm")]: { display: "none" },
+  },
+  title: {
+    flexGrow: 1,
+    fontFamily: "Merienda One",
+  },
+  image: {
+    borderRadius: "50%",
+    width: "5%",
+    marginBottom: "-1.5%",
+    marginRight: "2%",
+  },
+  buttons: {
+    textDecoration: "none",
+    marginRight: "2%",
+  },
+  drawer: {
+    width: 240,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: 240,
+  },
+  toolbar: theme.mixins.toolbar,
+}));
+
+export default function NavBar() {
+  const classes = useStyles();
+
+  const [openApp, setOpenApp] = useState(false);
+
+  function openAction() {
+    setOpenApp(!openApp);
+  }
+  return (
+    <AppBar className={classes.appBar}>
+      <Toolbar>
+        <Typography variant="h6" className={classes.title}>
+          <Link to="/">
+            <img src={Logo} className={classes.image} />
+          </Link>
+          Healthier Club
+        </Typography>
+        <Link to="/products" className={classes.buttons}>
+          <Button color="secondary">Productos</Button>
+        </Link>
+        <Link to="/recipes" className={classes.buttons}>
+          <Button color="secondary">Recetas</Button>
+        </Link>
+
+        <Hidden only={["xs", "sm"]}>
+          <SearchBar />
+        </Hidden>
+
+        <Hidden only={["md", "lg", "xl"]}>
+          <IconButton color="secondary" onClick={openAction}>
+            <MenuIcon />
+          </IconButton>
+        </Hidden>
+
+
+        <Drawer
+          className={classes.drawer}
+          classes={{ paper: classes.drawerPaper }}
+          anchor="right"
+          open={openApp}
+          onClose={openAction}
+        >
+          <div className={classes.toolbar}></div>
+          <Divider />
+          <List />
+        </Drawer>
+
+
+      </Toolbar>
+    </AppBar>
+  );
 }
-
