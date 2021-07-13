74baa843871c5c3a90313b41ad201a17c2cecffa
=sachacuna <=>
2021-07-12 22:52:49 -0300

    feat update products back end

--- api/src/routes/products.js
+++ api/src/routes/products.js
@@ -4,6 +4,7 @@ const express = require("express");
 const addProduct = require("../controllers/products/addProductController");
 const putProduct = require("../controllers/products/updateProductController");
 const deleteProduct = require("../controllers/products/deleteProductController");
+const updateProduct = require("../controllers/products/updateProductController");
 const getAllProducts = require("../controllers/products/getProductController");
 const getProductById = require("../controllers/products/getProductByIdController");
 
@@ -14,5 +15,6 @@ router.get('/:id', getProductById);
 router.post('/', addProduct);
 router.put('/:id', putProduct);
 router.delete('/:id', deleteProduct); // JM  esto lo rompio ed y cia. 
+router.put('/:id', updateProduct); 
 
 module.exports = router;
