2ead3f932ef27414671723f2f5fc03aa8f1a83b4
caszofficial <santiagozapata07@gmail.com>
2021-07-16 23:33:32 -0500

    deploy settings

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,10 @@ import AppGlobal from './AppGlobal.jsx';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import dotenv from "dotenv";
+dotenv.config();
 
+axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}> 
