6badd8802abc06dbe8b451234467ad3237d26b24
caszofficial <santiagozapata07@gmail.com>
2021-07-18 22:53:17 -0500

    minor changes for vercel

--- api/src/utils/constants.js
+++ api/src/utils/constants.js
@@ -1,5 +1,5 @@
 const API_DOMAIN = 'https://healthierclub.herokuapp.com'
-const CLIENT_DOMAIN = 'healthier.vercel.app'
+const CLIENT_DOMAIN = 'https://healthier.vercel.app'
 
 module.exports = {
     API_DOMAIN,
