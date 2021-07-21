85fbb40298fe3390cf6b8c2d877ed37f8245cc3e
caszofficial <santiagozapata07@gmail.com>
2021-07-21 00:45:30 -0500

    test deploy

--- api/src/db.js
+++ api/src/db.js
@@ -9,7 +9,7 @@ let sequelize =
     ? new Sequelize({
         database: process.env.DB_NAME,
         dialect: "postgres",
-        host: process.env.DB_HOST,
+        host: "ec2-52-45-183-77.compute-1.amazonaws.com",
         port: 5432,
         username: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
